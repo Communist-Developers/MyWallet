91d904e2d616fa0a9c2e37df783c83a815872704
Alireza <iranexplosm@gmail.com>
2024-12-14 14:19:32 +0330

    added

--- js/app.js
+++ js/app.js
@@ -1,18 +1,46 @@
-let totalIncome = prompt('Please Add Your Weekly Income');
+'use strict'
 
-if(totalIncome !== ''){
-    document.querySelector('#total').textContent = totalIncome;
-    document.querySelector('#left').textContent = totalIncome
-}else{
-    while(totalIncome == ''){
-        totalIncome = prompt('Please Add Your Weekly Income');
-        if(totalIncome !== ''){
-            document.querySelector('#total').textContent = totalIncome;
-            document.querySelector('#left').textContent = totalIncome
-        }
-    }
-}
 
+// let incomeEntry = prompt('please gimme some cash')
+// let incomeNum = Number(incomeEntry)
+// console.log(incomeNum, typeof incomeNum);
+
+
+
+
+// if(typeof incomeNum === 'number') {
+//     console.log('number');
+// } else {
+//     console.log('not only number');
+// }
+
+
+// HTML Refrences
+// Name for Cost
+let expense = document.querySelector('#expense')
+// Cash cost
+let amount = document.querySelector('#amount')
+// Entry cash
+let total = document.querySelector('#total')
+console.log(total);
+// Left Cash
+let left = document.querySelector('#left')
+// Bnt in order to add Cash
+let addingBtn = document.querySelector('.addingBtn')
+// List
+let list = document.querySelector('.list-group')
+
+let totalAmount = 20
+
+
+// function addToTotal() {
+//     total.push(totalAmount)
+// }
+
+total.innerHTML = totalAmount
+console.log(total);
+
+console.log(total);
 
 
 
