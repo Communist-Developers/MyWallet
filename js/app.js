1ae4e845b9a23e27f6566387ecb32a774f730a9c
Alireza <iranexplosm@gmail.com>
2024-12-14 18:06:14 +0330

    js added(features reamaining)

--- js/app.js
+++ js/app.js
@@ -1,25 +1,11 @@
 'use strict'
 
-
-// let incomeEntry = prompt('please gimme some cash')
-// let incomeNum = Number(incomeEntry)
-// console.log(incomeNum, typeof incomeNum);
-
-
-
-
-// if(typeof incomeNum === 'number') {
-//     console.log('number');
-// } else {
-//     console.log('not only number');
-// }
-
-
-// HTML Refrences
+//// HTML Refrences
 // Name for Cost
-let expense = document.querySelector('#expense')
+let expenseInput = document.querySelector('#expense')
+console.log(expense);
 // Cash cost
-let amount = document.querySelector('#amount')
+let amountInput = document.querySelector('#amount')
 // Entry cash
 let total = document.querySelector('#total')
 console.log(total);
@@ -27,20 +13,55 @@ console.log(total);
 let left = document.querySelector('#left')
 // Bnt in order to add Cash
 let addingBtn = document.querySelector('.addingBtn')
-// List
+// UL
 let list = document.querySelector('.list-group')
 
-let totalAmount = 20
 
 
-// function addToTotal() {
-//     total.push(totalAmount)
-// }
 
-total.innerHTML = totalAmount
-console.log(total);
+let incomeNum
+do {
+    incomeNum = Number(prompt('Gimme Some Cash u Lil Ni**a'))
+    
+} while (isNaN(incomeNum) || incomeNum <= 0);
 
-console.log(total);
+
+
+let totalEntry = incomeNum
+total.innerHTML = incomeNum
+left.innerHTML = totalEntry
+
+
+
+addingBtn.addEventListener('click' , function(e) {
+    e.preventDefault()
+
+    if (expenseInput.value == ''|| amountInput.value == '' ) {
+        alert('Empty Inputs. \n (mke sure to enter only numbers in cost section)')
+    } else {
+        let expense = expenseInput.value
+        let amount = parseFloat(amountInput.value)
+
+            console.log('ONly Number'); 
+            math()
+            addToList(expense, amount)
+            expenseInput.value = ''
+            amountInput.value = ''               
+    }
+})
+
+
+function math() {
+    totalEntry -= amount.value
+    left.innerHTML = totalEntry
+    // console.log(amount.value);
+}
+
+
+function addToList(expense, amount) {
+    list.innerHTML += `<li>${expense}:  ==> $${amount}</li>`
+}
+
 
 
 
